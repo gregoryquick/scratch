1dffd77e4743f50798c40e0bd2abab8b7776bf65
gregoryquick <gregory@quicks.me>
2020-03-20 17:41:44 -0400

    Changed world data signature
    Made neccisary changes inn main, and moved Starting world to structure for convienece

--- app/src/Structure.hs
+++ app/src/Structure.hs
@@ -6,36 +6,20 @@ import Data.Ord
 import Data.Semigroup
 
 --Continous world structure
-data World = World Double Double deriving Show
+data World = World (Double -> Double)
 instance Semigroup World where
-  (<>) (World x0 p0) (World x1 p1) = World (x0+x1) (p0+p1)
-getX :: World -> Double
-getX (World x _) = x
-setX :: World -> Double -> World
-setX (World _ p) x = World x p
-getP :: World -> Double
-getP (World _ p) = p
-setP :: World -> Double -> World
-setP (World x _) p = World x p
-multiply :: Double -> World -> World
-multiply s (World x p) = World (s*x) (s*p)
+  (<>) (World f) (World g) = World (\x -> (f x)+(g x))
 -- Length of string in meters
 -- length = pi
 -- unitMass = c*objectMass
 -- tension = ((l^2)/(pi^2))*c*springConstant
 
--- Simple pendulum differential equation
--- g = -9.8
--- l = 1.0
--- derivitive :: World -> World
--- derivitive (World x p) = World (p) ((g/l)*sin((pi/180)*x))
+startingWorld :: World
+startingWorld = World (\x -> 0.0)
 
 --Integrator for now asumed to be of form y = y0 + h * z
 timeStep :: Double
 timeStep = 0.5
-
-getList = [getX, getP]
-setList = [setX, setP]
 
 nodes :: Int
 nodes = 2
